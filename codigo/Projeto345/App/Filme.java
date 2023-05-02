f58fa990609418941165a51de3a0ddcf010ee651
Luiz Gustavo <104042158+Luizgms@users.noreply.github.com>
2023-05-02 10:20:04 -0300

    Projeto 4 especificação 1

---
+++ codigo/Projeto345/App/Filme.java
@@ -0,0 +1,30 @@
+public class Filme extends Conteudo {
+
+    private int duracao;
+
+    public Filme(int id, String nome, String dataDeLancamento, int duracao) {
+        super(id, nome, dataDeLancamento);
+        setDuracao(duracao);
+    }
+
+    public Filme(int duracao) {
+        setDuracao(duracao);
+    }
+
+    public Filme() {
+    }
+
+    public int getDuracao() {
+        return this.duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        if (duracao < 0) {
+            System.out.println("AVISO: a duração deve ser mais que 0");
+            return;
+        }
+
+        this.duracao = (duracao * 60);
+    }
+
+}
---
+++ codigo/Projeto345/App/Serie.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+public class Serie extends Conteudo {
+    private final static Random gerador = new Random();
+
+    private int quantidadeEpisodio;
+
+    public Serie(String nome, String dataDeLancamento, int quantidadeEpisodio) {
+        super(nome, dataDeLancamento);
+        setQuantidadeEpisodios(quantidadeEpisodio);
+    }
+
+    public Serie(int id, String nome, String dataDeLancamento) {
+        super(id, nome, dataDeLancamento);
+        int quantidadeEpisodio = gerador.nextInt(100);
+        setQuantidadeEpisodios(quantidadeEpisodio);
+    }
+
+    public Serie(int quantidadeEpisodio) {
+        setQuantidadeEpisodios(quantidadeEpisodio);
+    }
+
+    public Serie() {
+    }
+  
+    public int getQuantidadeEpisodios() {
+      return this.quantidadeEpisodio;
+    }
+
+    public void setQuantidadeEpisodios(int quantidadeEpisodio) {
+      if (quantidadeEpisodio < 0) {
+        System.out.println("AVISO: a quantidade de episodios deve ser maior que 0");
+        return;
+      }
+
+        this.quantidadeEpisodio = quantidadeEpisodio;
+    }
+
+}
